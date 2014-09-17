55df4b21629306d3d75adcf40ffa7b3255877d57
ameliebibi <billioud@MacBook-Pro-de-BILLIOUD.local>
2014-09-17 18:17:22 +0200

    maj 18:16
    mise à jour avec widget ratina
    et formulaire sur le visuel en superposition
    gracias

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 		<link href='http://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Ubuntu:300' rel='stylesheet' type='text/css'>
 
 
 		<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.min.css">
---
+++ searchform.php
@@ -0,0 +1,6 @@
+<form role="search" method="get" id="searchform" class="searchform" action="<?php esc_url( home_url( '/' ) ); ?>">
+	<div>
+		<input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" />
+		<input type="submit" id="searchsubmit" class="btn btn-primary btn-lg" value="OK"/> 
+	</div>
+</form>
